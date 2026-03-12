fb60e5f50bc24db0766b06c4f37405152663e7ac
kielbassa <karol.narel@icloud.com>
2026-03-12 20:11:16 +0100

    bluetooth communication

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
+//Bluetooth libraries
+#include "BluetoothSerial.h"
+#include <string.h>
+
 //OLED libraries
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <graphics.h>
 
 //OLED pins
 #define OLED_SDA 4
@@ -11,13 +14,18 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
+//other files
+#include <graphics.h>
+
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
+BluetoothSerial SerialBT;
 
 void setup() {
   Serial.begin(115200);
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
   
-
   //reset OLED display via software
   pinMode(OLED_RST, OUTPUT);
   digitalWrite(OLED_RST, LOW);
@@ -27,27 +35,42 @@ void setup() {
   //initialize OLED and display buffers
   Wire.begin(OLED_SDA, OLED_SCL);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false);
-  display.setTextSize(1);
-  display.setTextWrap(1);
-  display.setTextColor(WHITE);
-
-  display.clearDisplay();
-  display.clearDisplay();
   
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.drawBitmap(0,0,epd_bitmap_bunnytransmssion,64,64,WHITE);
+  display.setCursor(64, 10);
+  display.println("Waiting");
+  display.setCursor(64, 18);
+  display.println("for BT...");
+  display.display();
+
+  // Initialize Bluetooth
+  SerialBT.begin("ESP32_OLED"); // Bluetooth name
+  Serial.println("Device started, pairing allowed");
+
 }
 
-uint32_t iter = 0;
+uint8_t iter = 0;
+String receivedText = "";
 
 void loop() {
-  if (iter==192){
-    iter=0;
+  if (SerialBT.available()) {
+    char incomingChar = SerialBT.read();
+    
+    if (incomingChar != '\n'){ // If not newline, build string
+      receivedText += String(incomingChar);
+    } else {
+      // Display received text
+      Serial.println(receivedText);
+      display.clearDisplay();
+      display.setCursor(0,10);
+      display.setTextSize(2);
+      display.println(receivedText);
+      display.display();
+      receivedText = ""; // Clear string
+    }
   }
-  display.clearDisplay();
-  display.drawBitmap(iter-64, 0, epd_bitmap_bunnytransmssion, 64, 64, WHITE);
-
-  display.setCursor(16,28);
-  display.print("Radio Transmission");
-
-  display.display();
-  iter+=2;
+  delay(20);
 }
